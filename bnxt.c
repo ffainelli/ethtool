ffainelli/ethtool

commit a2f813eeecf1493de0a8d4df365c6c7b0818ab2d

bnxt: Add Broadcom driver support.
This patch adds the initial support for parsing registers dumped
by the Broadcom driver. Currently, PXP and PCIe registers are
parsed.

Reviewed-by: Andy Gospodarek <gospo@broadcom.com>
Reviewed-by: Edwin Peer <edwin.peer@broadcom.com>
Signed-off-by: Vasundhara Volam <vasundhara-v.volam@broadcom.com>
Signed-off-by: Michal Kubecek <mkubecek@suse.cz>

bnxt.c
@@ -1,0 +1,97 @@
+/* Code to dump registers for NetXtreme-E/NetXtreme-C Broadcom devices.
+ *
+ * Copyright (c) 2020 Broadcom Inc.
+ */
+#include <stdio.h>
+#include "internal.h"
+
+#define BNXT_PXP_REG_LEN	0x3110
+#define BNXT_PCIE_STATS_LEN	(12 * sizeof(u64))
+
+struct bnxt_pcie_stat {
+	const char *name;
+	u16 offset;
+	u8 size;
+	const char *format;
+};
+
+static const struct bnxt_pcie_stat bnxt_pcie_stats[] = {
+	{ .name = "PL Signal integrity errors", .offset = 0, .size = 4, .format = "%llu" },
+	{ .name = "DL Signal integrity errors", .offset = 4, .size = 4, .format = "%llu" },
+	{ .name = "TLP Signal integrity errors", .offset = 8, .size = 4, .format = "%llu" },
+	{ .name = "Link integrity", .offset = 12, .size = 4, .format = "%llu" },
+	{ .name = "TX TLP traffic rate", .offset = 16, .size = 4, .format = "%llu" },
+	{ .name = "RX TLP traffic rate", .offset = 20, .size = 4, .format = "%llu" },
+	{ .name = "TX DLLP traffic rate", .offset = 24, .size = 4, .format = "%llu" },
+	{ .name = "RX DLLP traffic rate", .offset = 28, .size = 4, .format = "%llu" },
+	{ .name = "Equalization Phase 0 time(ms)", .offset = 33, .size = 1, .format = "0x%x" },
+	{ .name = "Equalization Phase 1 time(ms)", .offset = 32, .size = 1, .format = "0x%x" },
+	{ .name = "Equalization Phase 2 time(ms)", .offset = 35, .size = 1, .format = "0x%x" },
+	{ .name = "Equalization Phase 3 time(ms)", .offset = 34, .size = 1, .format = "0x%x" },
+	{ .name = "PHY LTSSM Histogram 0", .offset = 36, .size = 2, .format = "0x%x"},
+	{ .name = "PHY LTSSM Histogram 1", .offset = 38, .size = 2, .format = "0x%x"},
+	{ .name = "PHY LTSSM Histogram 2", .offset = 40, .size = 2, .format = "0x%x"},
+	{ .name = "PHY LTSSM Histogram 3", .offset = 42, .size = 2, .format = "0x%x"},
+	{ .name = "Recovery Histogram 0", .offset = 44, .size = 2, .format = "0x%x"},
+	{ .name = "Recovery Histogram 1", .offset = 46, .size = 2, .format = "0x%x"},
+};
+
+int bnxt_dump_regs(struct ethtool_drvinfo *info __maybe_unused, struct ethtool_regs *regs)
+{
+	const struct bnxt_pcie_stat *stats = bnxt_pcie_stats;
+	u16 *pcie_stats, pcie_stat16;
+	u32 reg, i, pcie_stat32;
+	u64 pcie_stat64;
+
+	if (regs->len < BNXT_PXP_REG_LEN) {
+		fprintf(stdout, "Length too short, expected at least 0x%x\n",
+			BNXT_PXP_REG_LEN);
+		return -1;
+	}
+
+	fprintf(stdout, "PXP Registers\n");
+	fprintf(stdout, "Offset\tValue\n");
+	fprintf(stdout, "------\t-------\n");
+	for (i = 0; i < BNXT_PXP_REG_LEN; i += sizeof(reg)) {
+		memcpy(&reg, &regs->data[i], sizeof(reg));
+		if (reg)
+			fprintf(stdout, "0x%04x\t0x%08x\n", i, reg);
+	}
+	fprintf(stdout, "\n");
+
+	if (!regs->version)
+		return 0;
+
+	if (regs->len < (BNXT_PXP_REG_LEN + BNXT_PCIE_STATS_LEN)) {
+		fprintf(stdout, "Length is too short, expected 0x%lx\n",
+			BNXT_PXP_REG_LEN + BNXT_PCIE_STATS_LEN);
+		return -1;
+	}
+
+	pcie_stats = (u16 *)(regs->data + BNXT_PXP_REG_LEN);
+	fprintf(stdout, "PCIe statistics:\n");
+	fprintf(stdout, "----------------\n");
+	for (i = 0; i < ARRAY_SIZE(bnxt_pcie_stats); i++) {
+		fprintf(stdout, "%-30s : ", stats[i].name);
+		switch (stats[i].size) {
+		case 1:
+			pcie_stat16 = 0;
+			memcpy(&pcie_stat16, &pcie_stats[stats[i].offset], sizeof(u16));
+			fprintf(stdout, stats[i].format, pcie_stat16);
+			break;
+		case 2:
+			pcie_stat32 = 0;
+			memcpy(&pcie_stat32, &pcie_stats[stats[i].offset], sizeof(u32));
+			fprintf(stdout, stats[i].format, pcie_stat32);
+			break;
+		case 4:
+			pcie_stat64 = 0;
+			memcpy(&pcie_stat64, &pcie_stats[stats[i].offset], sizeof(u64));
+			fprintf(stdout, stats[i].format, pcie_stat64);
+			break;
+		}
+		fprintf(stdout, "\n");
+	}
+
+	return 0;
+}

Makefile.am
@@ -17,7 +17,7 @@ ethtool_SOURCES += \
 		  smsc911x.c at76c50x-usb.c sfc.c stmmac.c	\
 		  sff-common.c sff-common.h sfpid.c sfpdiag.c	\
 		  ixgbevf.c tse.c vmxnet3.c qsfp.c qsfp.h fjes.c lan78xx.c \
-		  igc.c qsfp-dd.c qsfp-dd.h
+		  igc.c qsfp-dd.c qsfp-dd.h bnxt.c
 endif
 
 if ENABLE_BASH_COMPLETION

ethtool.c
@@ -1072,6 +1072,7 @@ static const struct {
 	{ "dsa", dsa_dump_regs },
 	{ "fec", fec_dump_regs },
 	{ "igc", igc_dump_regs },
+	{ "bnxt_en", bnxt_dump_regs },
 #endif
 };
 

internal.h
@@ -396,4 +396,7 @@ int fec_dump_regs(struct ethtool_drvinfo *info, struct ethtool_regs *regs);
 /* Intel(R) Ethernet Controller I225-LM/I225-V adapter family */
 int igc_dump_regs(struct ethtool_drvinfo *info, struct ethtool_regs *regs);
 
+/* Broadcom Ethernet Controller */
+int bnxt_dump_regs(struct ethtool_drvinfo *info, struct ethtool_regs *regs);
+
 #endif /* ETHTOOL_INTERNAL_H__ */

commit cbd784424577a6df7a7592e9904a22b5c5d599b7

fix a warning when compiling for 32-bit
Since BNXT_PCIE_STATS_LEN is size_t:

../../ethtool/bnxt.c:66:68: warning: format ‘%lx’ expects argument of type ‘long unsigned int’, but argument 3 has type ‘unsigned int’ [-Wformat=]
   66 |                 fprintf(stdout, "Length is too short, expected 0x%lx\n",
      |                                                                  ~~^
      |                                                                    |
      |                                                                    long unsigned int
      |                                                                  %x

Fixes: a2f813eeecf1 ("bnxt: Add Broadcom driver support.")
Signed-off-by: Sergei Antonov <saproj@gmail.com>

bnxt.c
@@ -63,7 +63,7 @@ int bnxt_dump_regs(struct ethtool_drvinfo *info __maybe_unused, struct ethtool_r
 		return 0;
 
 	if (regs->len < (BNXT_PXP_REG_LEN + BNXT_PCIE_STATS_LEN)) {
-		fprintf(stdout, "Length is too short, expected 0x%lx\n",
+		fprintf(stdout, "Length is too short, expected 0x%zx\n",
 			BNXT_PXP_REG_LEN + BNXT_PCIE_STATS_LEN);
 		return -1;
 	}